e97b9ea25e15d70c7558f6d36503ee1e17860673
For-testing <129156990+Murodillo-dev@users.noreply.github.com>
2024-08-16 12:25:01 +0500

    .

---
+++ api/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loop from './Carousel/Loop'
+
+const App = () => {
+  return (
+    <div>
+      <Loop/>
+    </div>
+  )
+}
+
+export default App
---
+++ api/src/Carousel/Loop.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import rasm from '../Carousel/img.jpg'
+
+const Loop = () => {
+    return (
+        <div id='app'>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                autoplay={{
+                    delay: 1000,
+                    disableOnInteraction: false,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide >
+                    <div className='blok' style={{ background: `url(${rasm})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div style={{ background: `url(${rasm})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+                </SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+        </div>
+    )
+}
+
+export default Loop
